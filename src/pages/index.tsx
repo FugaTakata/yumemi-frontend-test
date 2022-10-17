32bc1cdc51075be8694cc33f056bb6a8ca4d9809
FugaTakata <maple1213goo@gmail.com>
2022-10-17 14:08:42 +0900

    style: 💄 formatting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>Hello Next!</div>
-}
+  return <div>Hello Next!</div>;
+};
 
-export default Home
+export default Home;
