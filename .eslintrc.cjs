94586be2df6cd0a08040ba2e06b3adf48d8320b7
FugaTakata <maple1213goo@gmail.com>
2022-10-17 14:19:52 +0900

    style: 💄 formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,25 +2,17 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard',
-    'next/core-web-vitals'
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ["plugin:react/recommended", "standard", "next/core-web-vitals"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
-  rules: {
-  }
-}
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
+};
