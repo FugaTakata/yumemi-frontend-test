994ae54398049fd61be550b247f6b09786d89507
FugaTakata <maple1213goo@gmail.com>
2022-10-18 14:13:09 +0900

    refactor: 💡 外部APIとの通信部分をapi routesにまとめた

--- src/features/prefectures/api/getPrefectures.ts
+++ src/features/prefectures/api/getPrefectures.ts
@@ -1,24 +1,9 @@
-import { API_HOST } from "@src/config/const";
-import { SECRET_RESAS_API_KEY } from "@src/config/env";
+import type { Prefecture } from "@src/features/prefectures/types";
 
-const API_ENDPOINT = API_HOST + "/api/v1/prefectures";
+export const getPrefecturesApiEndpoint = "/api/prefectures";
 
-interface GetPrefecturesResponse {
-  message: null;
-  result: {
-    prefCode: number;
-    prefName: string;
-  }[];
-}
-
-export const getPrefectures = async (): Promise<GetPrefecturesResponse> => {
-  const headers: RequestInit["headers"] = {
-    "X-API-KEY": SECRET_RESAS_API_KEY,
-  };
-
-  const response = await fetch(API_ENDPOINT, {
-    headers,
-  });
+export const getPrefectures = async (): Promise<Prefecture[]> => {
+  const response = await fetch(getPrefecturesApiEndpoint);
 
   const data = await response.json();
 
