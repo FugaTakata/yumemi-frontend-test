e0b26e7d7ecd57cee41b09f22ed0ea2b83c282a6
FugaTakata <maple1213goo@gmail.com>
2022-10-19 19:40:18 +0900

    feat: 🎸 都道府県チェックボックス管理hooksの作成
    render hookでの都道府県管理hook作成

---
+++ src/features/prefectures/hooks/usePrefectureCheckbox.tsx
@@ -0,0 +1,103 @@
+import type { ChangeEventHandler } from "react";
+import { memo, useCallback, useState } from "react";
+
+import { css } from "@emotion/react";
+
+import { Checkbox } from "@src/components/Elements/Checkbox";
+import type { Prefecture } from "@src/features/prefectures/types";
+
+const listStyle = css`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const listItemStyle = css`
+  min-width: 6rem;
+`;
+
+interface PrefectureCheckboxListItemProps {
+  prefecture: Prefecture;
+  isChecked: boolean;
+  onCheckToggle: (prefectureId: Prefecture["id"]) => void;
+}
+
+const PrefectureCheckboxListItem = memo(
+  function PrefectureCheckboxListItemMemo({
+    prefecture,
+    isChecked,
+    onCheckToggle,
+  }: PrefectureCheckboxListItemProps) {
+    const handleCheckToggle: ChangeEventHandler<HTMLInputElement> = useCallback(
+      (_e) => {
+        onCheckToggle(prefecture.id);
+      },
+      [prefecture, onCheckToggle]
+    );
+
+    return (
+      <li css={listItemStyle} key={prefecture.id}>
+        <Checkbox
+          checked={isChecked}
+          label={prefecture.name}
+          onCheckToggle={handleCheckToggle}
+        />
+      </li>
+    );
+  }
+);
+
+interface UsePrefectureCheckboxProps {
+  prefectures: Prefecture[] | undefined;
+}
+
+export const usePrefectureCheckbox = ({
+  prefectures,
+}: UsePrefectureCheckboxProps) => {
+  const [checkedIds, setCheckedIds] = useState<Prefecture["id"][]>([]);
+
+  const handleCheckToggle = useCallback(
+    (prefectureId: Prefecture["id"]): void => {
+      setCheckedIds((prevCheckedIds) => {
+        if (prevCheckedIds.includes(prefectureId)) {
+          return prevCheckedIds.filter(
+            (prevCheckedId) => prevCheckedId !== prefectureId
+          );
+        }
+
+        return [...prevCheckedIds, prefectureId];
+      });
+    },
+    []
+  );
+
+  const renderCheckboxList = useCallback((): JSX.Element => {
+    if (prefectures === undefined) {
+      return <p>loading...</p>;
+    }
+
+    return (
+      <ul css={listStyle}>
+        {prefectures.map((prefecture) => {
+          const isChecked = checkedIds.includes(prefecture.id);
+
+          return (
+            <PrefectureCheckboxListItem
+              isChecked={isChecked}
+              key={prefecture.id}
+              prefecture={prefecture}
+              onCheckToggle={handleCheckToggle}
+            />
+          );
+        })}
+      </ul>
+    );
+  }, [prefectures, checkedIds, handleCheckToggle]);
+
+  return {
+    checkedIds,
+    renderCheckboxList,
+  };
+};
