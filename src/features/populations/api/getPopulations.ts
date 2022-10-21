9ae46697b37042534d0e87f0fe626fe7624ad0b7
FugaTakata <maple1213goo@gmail.com>
2022-10-22 04:06:49 +0900

    refactor: 💡 引数をprefectureIdに変更

--- src/features/populations/api/getPopulations.ts
+++ src/features/populations/api/getPopulations.ts
@@ -1,7 +1,11 @@
 import type { Population } from "@src/features/populations/types";
 
-export const getPopulations = async (url: string): Promise<Population[]> => {
-  const response = await fetch(url);
+export const getPopulations = async ({
+  prefectureId,
+}: {
+  prefectureId: number;
+}): Promise<Population[]> => {
+  const response = await fetch(`/api/prefectures/${prefectureId}/population`);
 
   const data = await response.json();
 
