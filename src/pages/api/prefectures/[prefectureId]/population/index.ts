5c90bbebe85e3d127e1fd6e47904f7caa26430b7
FugaTakata <maple1213goo@gmail.com>
2022-10-21 23:59:16 +0900

    fix: 🐛 型情報のタイポを修正

--- src/pages/api/prefectures/[prefectureId]/population/index.ts
+++ src/pages/api/prefectures/[prefectureId]/population/index.ts
@@ -11,7 +11,7 @@ interface GetPopulationsResponse {
   result: {
     boundaryYear: number;
     data: {
-      label: "総人口" | "年少人口" | "生産年齢  人口" | "老年人口";
+      label: "総人口" | "年少人口" | "生産年齢人口" | "老年人口";
       data: {
         year: number;
         value: number;
