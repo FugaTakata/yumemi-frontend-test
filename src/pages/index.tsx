3f8a4cd1cf8568ad7c1c4cd847333588935d6770
FugaTakata <maple1213goo@gmail.com>
2022-10-22 12:51:10 +0900

    feat: 🎸 折れ線図の表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,44 @@
+import { css } from "@emotion/react";
+
+import { PopulationsLineChart } from "@src/features/populations/components/PopulationsLineChart";
+import { usePopulationData } from "@src/features/populations/hooks/usePopulationData";
+import { usePopulationGraphData } from "@src/features/populations/hooks/usePopulationGraphData";
 import { usePrefectureCheckbox } from "@src/features/prefectures/hooks/usePrefectureCheckbox";
 import { usePrefectureData } from "@src/features/prefectures/hooks/usePrefectureData";
 
 import type { NextPage } from "next";
 
+const graphWrapperStyle = css`
+  height: 600px;
+  overflow-x: scroll;
+`;
+
 const Home: NextPage = () => {
   const { prefectures } = usePrefectureData();
 
-  const { renderCheckboxList } = usePrefectureCheckbox({
+  const { checkedIds, renderCheckboxList } = usePrefectureCheckbox({
     prefectures,
   });
 
-  return <div>{renderCheckboxList()}</div>;
+  const { populations } = usePopulationData({
+    checkedPrefectureIds: checkedIds,
+  });
+
+  const { graphData } = usePopulationGraphData({
+    populations,
+    prefectures,
+  });
+
+  console.log(populations);
+
+  return (
+    <div>
+      {renderCheckboxList()}
+      <div css={graphWrapperStyle}>
+        <PopulationsLineChart graphData={graphData} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
