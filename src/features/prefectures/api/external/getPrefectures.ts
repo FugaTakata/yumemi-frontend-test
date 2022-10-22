05c8e8ef6fcac49ca6100c26c39ea75f439999cf
Fuga Takata <50868338+FugaTakata@users.noreply.github.com>
2022-10-22 16:47:22 +0900

    RESASへのリクエストの関数化 (#25)
    * feat: 🎸 RESASへのfetch関数を追加
    
    * refactor: 💡 関数化したRESASへのリクエスト処理の利用

--- src/features/prefectures/api/external/getPrefectures.ts
+++ src/features/prefectures/api/external/getPrefectures.ts
@@ -1,17 +1,10 @@
+import { fetchResas } from "@src/api/external/resas";
 import { API_HOST } from "@src/config/const";
-import { SECRET_RESAS_API_KEY } from "@src/config/env/server";
 import type { GetPrefecturesResponse } from "@src/features/prefectures/types";
 
-const API_ENDPOINT = API_HOST + "/api/v1/prefectures";
-
 export const getPrefectures = async (): Promise<GetPrefecturesResponse> => {
-  const headers: RequestInit["headers"] = {
-    "X-API-KEY": SECRET_RESAS_API_KEY,
-  };
-
-  const response = await fetch(API_ENDPOINT, {
-    headers,
-  });
+  const url = API_HOST + "/api/v1/prefectures";
+  const response = await fetchResas(url);
 
   const data = await response.json();
 
