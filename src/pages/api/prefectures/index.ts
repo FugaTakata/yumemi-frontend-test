886bbaba12e45e5990477752d436d41b86973af1
Fuga Takata <50868338+FugaTakata@users.noreply.github.com>
2022-10-23 14:46:49 +0900

    feat: 🎸 リクエストメソッドの区別を追加 (#39)

--- src/pages/api/prefectures/index.ts
+++ src/pages/api/prefectures/index.ts
@@ -3,13 +3,22 @@ import { convertToClientData } from "@src/features/prefectures/utils";
 
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(
-  _req: NextApiRequest,
-  res: NextApiResponse
-) {
+const getHandler = async (_req: NextApiRequest, res: NextApiResponse) => {
   const data = await getPrefectures();
 
   const clientFriendlyData = convertToClientData(data);
 
   res.status(200).json(clientFriendlyData);
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case "GET":
+      return getHandler(req, res);
+    default:
+      return res.status(405).json({ message: "Method Not Allowed" });
+  }
 }
