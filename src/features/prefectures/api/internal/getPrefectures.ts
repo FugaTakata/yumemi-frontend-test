23fe37a516a73abec18b05b1cfea3efd73b9942f
FugaTakata <maple1213goo@gmail.com>
2022-10-23 16:32:28 +0900

    feat: 🎸 statusが成功でなかった場合にerrorをthrowさせる

--- src/features/prefectures/api/internal/getPrefectures.ts
+++ src/features/prefectures/api/internal/getPrefectures.ts
@@ -5,7 +5,12 @@ export const getPrefecturesApiEndpoint = "/api/prefectures";
 export const getPrefectures = async (): Promise<Prefecture[]> => {
   const response = await fetch(getPrefecturesApiEndpoint);
 
-  const data = await response.json();
+  if (!response.ok) {
+    console.error(response);
+    throw new Error(response.statusText);
+  }
+
+  const data: Prefecture[] = await response.json();
 
   return data;
 };
